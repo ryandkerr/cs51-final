e5a210a6ac77ac94b656872ef3888af7ae8a8c37
Milan Ravenell <mravenell@college.harvard.edu>
2015-04-23 01:06:56 -0400

    terminal board

--- board_functions.py
+++ board_functions.py
@@ -5,12 +5,79 @@ class GameBoard:
 
 	Board = 
 
-	def horizontal_in_row:
+	def horizontal (state):
+		for y in range(columns):
+			in_row = 0
+			for x in range(rows):
+				if (Board[x][y]).state = state:
+					in_row = in_row + 1
+					if (in_row = 4):
+						return true
+				else:
+					in_row = 0
+		return false
+
+	def vertical (state):
+		for x in range(rows):
+			in_row = 0
+			for y in range(columns):
+				if (Board[x][y]).state = state:
+					in_row = in_row + 1
+					if (in_row = 4):
+						return true
+				else:
+					in_row = 0
+		return false
+
+	def diag_upright (state,x,y):
 		in_row = 0
+		while(x <= rows && y <= columns):
+			if ((Board[x][y]).state = state):
+				in_rows = in_rows + 1
+				if(in_row = 4):
+					return true
+				x = x + 1
+				y = y + 1
+			else: 
+				in_row = 0
+				x = x + 1
+				y = y + 1
+		return false
 
+	def diag_downright (state,x,y):
+		in_row = 0
+		while(x >= 0 && y >= 0):
+			if ((Board[x][y]).state = state):
+				in_rows = in_rows + 1
+				if(in_row = 4):
+					return true
+				x = x - 1
+				y = y - 1
+			else: 
+				in_row = 0
+				x = x - 1
+				y = y - 1
+		return false 
 
+	def diaganol (state):
+		for x in range(rows):
+			for y in range(columns):
+					if diag_upright(state, x, y) or diag_downright(state,x,y) :
+						return true
+		return false
 
-	def is_terminal board 
+		def full:
+			for x in range(rows):
+				for y in range(columns):
+					if ((Board[x][y]).state = "empty"):
+						return false
+			return true
+
+	def is_terminal board(turn):
+		if (horizontal(turn) or vertical(turn) or diagonal(turn) or full):
+			return true
+		else:
+			return false
 
 	def possible_moves 
 
@@ -23,6 +90,9 @@ class Piece:
 	self.r_pos = x_loc
 	self.c_pos = y_loc
 	self.state = state
+
+slot = Piece(0,0,"empty")
+Board = [[slot for x in range(rows)] for y in range(columns)]
 
 for x in range(rows):
 	for y in range(columns):
