a7980add00d9c50b5a7aff1ee7cad27489e18c2b
Milan Ravenell <mravenell@college.harvard.edu>
2015-04-22 23:17:22 -0400

    Board functions

---
+++ board_functions.py
@@ -0,0 +1,30 @@
+class GameBoard:
+	def __init__ (self):
+	self.rows = 7
+	self.columns = 5
+
+	Board = 
+
+	def horizontal_in_row:
+		in_row = 0
+
+
+
+	def is_terminal board 
+
+	def possible_moves 
+
+	def go_next  
+
+
+
+class Piece:
+	def __init__ (self,x_loc,y_loc,state):
+	self.r_pos = x_loc
+	self.c_pos = y_loc
+	self.state = state
+
+for x in range(rows):
+	for y in range(columns):
+		slot = Piece(x,y,"empty")
+		Matrix[x][y] = spot
