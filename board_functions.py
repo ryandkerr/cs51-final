d7476d1e58bd955dd9508849549a2e7e4fa07007
Ryan <ryankerr@college.harvard.edu>
2015-04-24 10:09:30 -0400

    tabs > spaces and added game_won

--- board_functions.py
+++ board_functions.py
@@ -1,108 +1,115 @@
 #class GameBoard:
-#	def __init__ (self):
-#	self.rows = 7
-#	self.columns = 5
+#   def __init__ (self):
+#   self.rows = 7
+#   self.columns = 5
 
 columns = 7
 rows = 6
 
 def horizontal (board, state):
-	for y in range(columns):
-		in_row = 0
-		for x in range(rows):
-			if (board[x][y] == state):
-				in_row = in_row + 1
-				if (in_row == 4):
-					return True
-			else:
-				in_row = 0
-	return False
+    for y in range(columns):
+        in_row = 0
+        for x in range(rows):
+            if (board[x][y] == state):
+                in_row = in_row + 1
+                if (in_row == 4):
+                    return True
+            else:
+                in_row = 0
+    return False
 
 def vertical (board, state):
-	for x in range(rows):
-		in_row = 0
-		for y in range(columns):
-			if (board[x][y] == state):
-				in_row = in_row + 1
-				if (in_row == 4):
-					return True
-			else:
-				in_row = 0
-	return False
+    for x in range(rows):
+        in_row = 0
+        for y in range(columns):
+            if (board[x][y] == state):
+                in_row = in_row + 1
+                if (in_row == 4):
+                    return True
+            else:
+                in_row = 0
+    return False
 
 def diag_upright (board, state,x,y):
-	in_row = 0
-	while(x <= rows and y <= columns):
-		if (board[x][y] == state):
-			in_rows = in_rows + 1
-			if(in_row == 4):
-				return True
-			x = x + 1
-			y = y + 1
-		else: 
-			in_row = 0
-			x = x + 1
-			y = y + 1
-	return False
+    in_row = 0
+    while(x <= rows and y <= columns):
+        if (board[x][y] == state):
+            in_rows = in_rows + 1
+            if(in_row == 4):
+                return True
+            x = x + 1
+            y = y + 1
+        else: 
+            in_row = 0
+            x = x + 1
+            y = y + 1
+    return False
 
 def diag_downright (board, state,x,y):
-	in_row = 0
-	while(x >= 0 and y >= 0):
-		if (board[x][y] == state):
-			in_rows = in_rows + 1
-			if(in_row == 4):
-				return True
-			x = x - 1
-			y = y - 1
-		else: 
-			in_row = 0
-			x = x - 1
-			y = y - 1
-	return False 
+    in_row = 0
+    while(x >= 0 and y >= 0):
+        if (board[x][y] == state):
+            in_rows = in_rows + 1
+            if(in_row == 4):
+                return True
+            x = x - 1
+            y = y - 1
+        else: 
+            in_row = 0
+            x = x - 1
+            y = y - 1
+    return False 
 
-def diaganol (board, state):
-	for x in range(rows):
-		for y in range(columns):
-				if diag_upright(board, state, x, y) or diag_downright(board, state,x,y) :
-					return True
-	return False
+def diagonal (board, state):
+    for x in range(rows):
+        for y in range(columns):
+                if diag_upright(board, state, x, y) or diag_downright(board, state,x,y) :
+                    return True
+    return False
 
 def full (board):
-	for x in range(rows):
-		for y in range(columns):
-			if (board[x][y] == "0"):
-				return False
-	return True
+    for x in range(rows):
+        for y in range(columns):
+            if (board[x][y] == "0"):
+                return False
+    return True
 
 def is_terminal (board, turn):
-	if (horizontal(board, turn) or vertical(board, turn) or diagonal(board, turn) or full(board)):
-		return True
-	else:
-		return False
+    if (horizontal(board, turn) or vertical(board, turn) or diagonal(board, turn) or full(board)):
+        return True
+    else:
+        return False
+
+def game_won (board, turn):
+    if (horizontal(board, turn) or vertical(board, turn) or diagonal(board, turn)):
+        return True
+    else:
+        return False
+
 
 def possible_moves (board):
-	moves = []
-	x = 0
-	y = 0
-	while(y < columns): 
-		while(x < rows):
-			if (board[x][y] == "0"):
-				moves.append(y) 
-				y = y + 1
-				x = 0
-			else:
-				x = x + 1
-		y = y + 1
-		x = 0
-	return moves
+    moves = []
+    x = 0
+    y = 0
+    while(y < columns): 
+        while(x < rows):
+            if (board[x][y] == "0"):
+                moves.append(y) 
+                y = y + 1
+                x = 0
+            else:
+                x = x + 1
+        y = y + 1
+        x = 0
+    return moves
 
 def go_next (board, move, state):
-	board1 = board 
-	for x in range(rows):
-		if(board1[x][move] == "0"):
-			board1[x][move] = state
-			return board1
-	return board
+    board1 = board 
+    for x in range(rows):
+        if(board1[x][move] == "0"):
+            board1[x][move] = state
+            return board1
+    return board
 
 # TESTING
 
@@ -111,21 +118,21 @@ board0 = [["0" for y in range(columns)] for x in range(rows)]
 
 board1 = board0
 for x in range(rows):
-	for y in range(columns):
-		if (x%2 == 0):
-			board1[x][y] = "r"
-		else:
-			board1[x][y] = "b"
+    for y in range(columns):
+        if (x%2 == 0):
+            board1[x][y] = "r"
+        else:
+            board1[x][y] = "b"
 
 
 
 board2 = board0
 for y in range(columns):
-	for x in range(rows):
-		if (y%2 == 0):
-			board2[x][y] = "r"
-		else:
-			board2[x][y] = "b"
+    for x in range(rows):
+        if (y%2 == 0):
+            board2[x][y] = "r"
+        else:
+            board2[x][y] = "b"
 
 
 
@@ -153,26 +160,26 @@ board5[1][3] = "r"
 
 
 if (is_terminal(board0, "r") == False):
-	print "success0"
+    print "success0"
 if (is_terminal(board1, "r") == True):
-	print "success1"
+    print "success1"
 if (is_terminal(board2, "r") == True):
-	print "success2"
+    print "success2"
 if (is_terminal(board3, "r") == True):
-	print "success3"
+    print "success3"
 if (is_terminal(board4, "r") == True):
-	print "success4"
+    print "success4"
 if (full(board1) == True):
-	print "success5"
+    print "success5"
 moves1 = possible_moves(board0)
 moves2 = possible_moves(board1)
 for x in moves1:
-	print x
+    print x
 for x in moves2:
-	print x
+    print x
 board6 = go_next(board5, 5, "r")
 if (is_terminal(board6, "r")):
-	print "succes6"
+    print "succes6"
 
 
 
@@ -192,7 +199,7 @@ if (is_terminal(board6, "r")):
 
 
 #class Piece:
-#	def __init__ (self,x_loc,y_loc,state):
-#	self.r_pos = x_loc
-#	self.c_pos = y_loc
-#	self.state = state
+#   def __init__ (self,x_loc,y_loc,state):
+#   self.r_pos = x_loc
+#   self.c_pos = y_loc
+#   self.state = state
