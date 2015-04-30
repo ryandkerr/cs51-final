89b5d8ec77732bdd3dbd0af4f0ce25d38e9af784
Ryan <ryankerr@college.harvard.edu>
2015-04-30 14:26:38 -0400

    more changes to alpha-beta

--- ab_pruning.py
+++ ab_pruning.py
@@ -8,83 +8,87 @@ from board_functions import *
 from evaluate import *
 
 # minimax takes a board and a specified depth and outputs the best MOVE
-def minimax(board, depth):
+def minimax_ab(board, depth):
+  
+  # get array of possible moves 
+  next_moves = possible_moves(board)
+  best_move = next_moves[0]
+  best_score = float("-inf")
+
+  alpha = float("-inf")
+  beta = float("inf")
+
+  
+  # go through all of those boards
+  for move in next_moves:
     
-    # get array of possible moves 
-    next_moves = possible_moves(board)
-    best_move = next_moves[0]
-    best_score = float("-inf")
-    
-    # go through all of those boards
-    for move in next_moves:
-        
-        # create new board from move
-        new_board = go_next(board, move, "R")
+    # create new board from move
+    new_board = go_next(board, move, "R")
 
-        # call min on that new board
-        board_score = min_player(new_board, depth - 1) + (move % 4)
+    # call min on that new board
+    board_score = min_ab(new_board, depth - 1, alpha, beta) + (move % 4)
 
-        if board_score > best_score:
-            best_score = board_score
-            best_move = move
+    if board_score > best_score:
+      best_score = board_score
+      best_move = move
 
-    return best_move
+  return best_move
 
 
 # min player is given a board and choses the lowest option, returns a SCORE
 def min_player(board, depth):
-    if is_terminal(board, "R"):
-    
-        # this needs to say wether the terminal state is a draw, win, loss
-        return evaluate(board)
+  if is_terminal(board, "R"):
+  
+    # this needs to say wether the terminal state is a draw, win, loss
+    return evaluate(board)
 
-    else:
-        next_moves = possible_moves(board) 
-        min_score = float("inf")
+  else:
+    next_moves = possible_moves(board) 
+    min_score = float("inf")
 
-        # if end of tree evaluate scores
-        for move in next_moves:
-            new_board = go_next(board, move, "Y")
+    # if end of tree evaluate scores
+    for move in next_moves:
+      new_board = go_next(board, move, "Y")
 
-            board_score = 0
+      board_score = 0
 
-            if depth == 0:
-                board_score = evaluate(new_board)
-            else:
-                board_score = max_player(new_board, depth - 1)
+      if depth == 0:
+        board_score = evaluate(new_board)
+      else:
+        board_score = max_player(new_board, depth - 1)
 
-            if board_score < min_score:
-                min_score = board_score
+      if board_score < min_score:
+        min_score = board_score
 
-        return min_score
+    return min_score
 
 
 # max player picks max move and outputs SCORE
 def max_player(board, depth):
-    if is_terminal(board, "Y"):
-    
-        # this needs to say wether the terminal state is a draw, win, loss
-        return evaluate(board)
+  if is_terminal(board, "Y"):
+  
+    # this needs to say wether the terminal state is a draw, win, loss
+    return evaluate(board)
 
-    else:
-        next_moves = possible_moves(board) 
-        max_score = float("-inf")
+  else:
+    next_moves = possible_moves(board) 
+    max_score = float("-inf")
 
-        # if end of tree, evaluate scores
-        for move in next_moves:
-            new_board = go_next(board, move, "R")
+    # if end of tree, evaluate scores
+    for move in next_moves:
+      new_board = go_next(board, move, "R")
 
-            board_score = 0
+      board_score = 0
 
-            if depth == 0:
-                board_score = evaluate(new_board)
-            else:
-                board_score = min_player(new_board, depth - 1)
+      if depth == 0:
+        board_score = evaluate(new_board)
+      else:
+        board_score = min_player(new_board, depth - 1)
 
-            if board_score > max_score:
-                max_score = board_score
+      if board_score > max_score:
+        max_score = board_score
 
-        return max_score
+    return max_score
 
 
 # outside of minimax call
@@ -99,55 +103,56 @@ def max_player(board, depth):
 # returns a score
 # min_ab(board, 3, -inf, inf) returns a score
 def min_ab(board, depth, a, b):
-    if is_terminal(board, "R"):
-    
-        # this needs to say wether the terminal state is a draw, win, loss
-        return evaluate(board)
+  if is_terminal(board, "R"):
+  
+    # this needs to say wether the terminal state is a draw, win, loss
+    return evaluate(board)
 
-    else:
-        next_moves = possible_moves(board) 
-        beta = b
+  else:
+    next_moves = possible_moves(board) 
+    beta = b
 
-        # if end of tree evaluate scores
-        for move in next_moves:
-            new_board = go_next(board, move, "Y")
+    # if end of tree evaluate scores
+    for move in next_moves:
+      board_score = float("inf")
 
-            board_score = float("inf")
+      if depth == 0:
+        new_board = go_next(board, move, "Y")
+        board_score = evaluate(new_board)
 
-            if depth == 0:
-                board_score = evaluate(new_board)
-            elif alpha >= b:
-                board_score = max_ab(new_board, depth - 1, a, beta)
+      elif alpha < b:
+        new_board = go_next(board, move, "Y")
+        board_score = max_ab(new_board, depth - 1, a, beta)
 
-            if board_score < beta:
-                beta = board_score
+      if board_score < beta:
+        beta = board_score
 
-        return beta
+    return beta
 
 
 
 def max_ab(board, depth, a, b):
-    if is_terminal(board, "Y"):
-    
-        # this needs to say wether the terminal state is a draw, win, loss
-        return evaluate(board)
+  if is_terminal(board, "Y"):
+  
+    # this needs to say wether the terminal state is a draw, win, loss
+    return evaluate(board)
 
-    else:
-        next_moves = possible_moves(board) 
-        alpha = a
+  else:
+    next_moves = possible_moves(board) 
+    alpha = a
 
-        # if end of tree, evaluate scores
-        for move in next_moves:
-            new_board = go_next(board, move, "R")
+    # if end of tree, evaluate scores
+    for move in next_moves:
+      board_score = float("-inf")
 
-            board_score = float("-inf")
+      if depth == 0:
+        new_board = go_next(board, move, "R")
+        board_score = evaluate(new_board)
+      elif alpha < beta:
+        new_board = go_next(board, move, "R")
+        board_score = min_ab(new_board, depth - 1, alpha, beta)
 
-            if depth == 0:
-                board_score = evaluate(new_board)
-            else:
-                board_score = min_ab(new_board, depth - 1, alpha, beta)
+      if board_score > alpha:
+        alpha = board_score
 
-            if board_score > alpha:
-                alpha = board_score
-
-        return alpha
+    return alpha
