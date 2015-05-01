0e08a5bfd6890903855be8b4edbc1a39cf2c2ada
Evan Sandhoefner <evansandhoefner@college.harvard.edu>
2015-05-01 04:57:21 -0400

    style??????

--- connectFour.py
+++ connectFour.py
@@ -21,7 +21,7 @@ global difficulty
 # instantiate empty board
 board = [["." for row in range(ROWS)] for column in range(COLUMNS)]
 
-# takes input originating from user, exits program if input is "q"
+# take input originating from user, exit program if input is "q"
 def exit_if(user_input):
   if user_input == "q":
     sys.exit()
@@ -31,35 +31,46 @@ def exit_if(user_input):
 def init():
   global playersTurn
   global difficulty
+
   print "\nHello! My name is Rondo. Let's play Connect Four!"
+
   first = raw_input("You'll be yellow (Y). You can enter 'q' at any prompt "
                     "to quit.\nWould you like to go first? (y/n):\n").lower()
   while (first != "y") & (first != "n") & (first != "q"):
     first = raw_input("\nSorry, I don't understand! "
                       "Please type 'y' or 'n':\n").lower()
+
   exit_if(first)
+
   if first == "n":
     playersTurn = False
   else:
     playersTurn = True
-  difficulty = raw_input("\nI'm pretty good at this, so you might want me to go"
-                    " easy. \nPlease choose a difficulty from 1 (easiest) to 5 "
-                    "(hardest).\nFair warning: when I play hard, my turns take "
-                    "a while.\n").lower()
+
+  difficulty = raw_input("\nI'm pretty good at this, so you might want me to "
+                         "go easy.\nPlease choose a difficulty from 1 "
+                         "(easiest) to 5 (hardest).\nFair warning: when I play"
+                         " hard, my turns take a while.\n").lower()
   while difficulty not in ["1","2","3","4","5","q"]:
     difficulty = raw_input("\nSorry, I don't understand! Please"
                            " type '1', '2', '3', '4', or '5'.\n").lower()
+
   exit_if(difficulty)
+
   difficulty = int(difficulty)
+
   print "\nStarting board:"
   printBoard()
+
   move()
+
 
 # check for game over, make last move lowercase, reassign playersTurn,
 # call moveAI or movePlayer
 def move():
   global playersTurn
   global board
+
   if game_won(board, "R"):
     print "\nGame over! I win!"
     sys.exit()
@@ -69,12 +80,14 @@ def move():
   elif full(board):
     print "\nGame over! It's a tie!"
     sys.exit()
+
   for row in range(ROWS):
     for column in range(COLUMNS):
       if board[column][row] == "R":
         board[column][row] = "r"
       if board[column][row] == "Y":
         board[column][row] = "y"
+
   if playersTurn:
     playersTurn = False
     movePlayer()
@@ -82,26 +95,36 @@ def move():
     playersTurn = True
     moveAI()
 
+
 # sleep if Rondo too fast, compute Rondo's optimal move, print board,
 # call move()
 def moveAI():
   global board
+
   print "\nRondo is thinking...."
+
   if difficulty < 4:
     time.sleep(1)
+
   board = go_next(board, minimax_ab(board, difficulty, "R"), "R")
+
   print "Rondo's move:"
   printBoard()
+  
   move()
   
+
 # take user column input, alter board in memory, print board, call move()
 def movePlayer():
   column = raw_input("\nYour turn! Please choose a column (1-7):\n").lower()
   while column not in ["q","1","2","3","4","5","6","7"]:
-    column = raw_input("\nSorry, I don't understand! Please type '1', '2', '3',"
-                       " '4', '5', '6', or '7'.\n")
+    column = raw_input("\nSorry, I don't understand! Please type '1', '2',"
+                       " '3', '4', '5', '6', or '7'.\n")
+
   exit_if(column)
+
   column = int(column) - 1
+
   for row in range(ROWS):
     if board[column][row] == ".":
       board[column][row] = "Y"
@@ -112,14 +135,17 @@ def movePlayer():
   else:
     movePlayer()
 
+
 # print ASCII board to terminal window
 def printBoard():
   for column in range(COLUMNS):
     print str(1 + column),
   print ""
+
   for row in range(ROWS):
     for column in range(COLUMNS):
       print board[column][ROWS - row - 1],
     print ""
+
 
 init()
